9d50e524471d1c1efd6291b444283dd3943ae2ea
sieunnnn <wldsmtldsm65@gmail.com>
2024-12-13 14:11:35 +0900

    [백준] 골드 문제풀이

--- java/baekjoon/gold/PROB10026.java
+++ java/baekjoon/gold/PROB10026.java
@@ -1,4 +1,99 @@
 package gold;
 
+import java.util.*;
+import java.io.*;
+
 public class PROB10026 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    static final int RED = 1;
+    static final int BLUE = 2;
+    static final int GREEN = 3;
+
+    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {-1, 1, 0, 0};
+
+    static int N;
+    static int[][] graph;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        int result = solve();
+
+        changeGraph();
+        int blindResult = solve();
+
+        System.out.println(result + " " + blindResult);
+    }
+
+    public static void init() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        graph = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split("");
+
+            for (int j = 0; j < N; j++) {
+                if (input[j].equals("R"))
+                    graph[i][j] = RED;
+
+                else if (input[j].equals("B"))
+                    graph[i][j] = BLUE;
+
+                else if (input[j].equals("G"))
+                    graph[i][j] = GREEN;
+            }
+        }
+    }
+
+    public static int solve() {
+        visited = new boolean[N][N];
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    int target = graph[i][j];
+                    bfs(i, j, target);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void bfs (int y, int x, int target) {
+        Queue<int[]> nodes = new LinkedList<>();
+        nodes.add(new int[]{y, x});
+        visited[y][x] = true;
+
+        while (!nodes.isEmpty()) {
+            int[] current = nodes.poll();
+            int currentY = current[0];
+            int currentX = current[1];
+
+            for (int i = 0; i < 4; i++) {
+                int ny = currentY + dy[i];
+                int nx = currentX + dx[i];
+
+                if (ny < N && ny >= 0 && nx < N && nx >= 0) {
+                    if (!visited[ny][nx] && graph[ny][nx] == target) {
+                        visited[ny][nx] = true;
+                        nodes.add(new int[]{ny, nx});
+                    }
+                }
+            }
+        }
+    }
+
+    public static void changeGraph() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (graph[i][j] == GREEN)
+                    graph[i][j] = RED;
+            }
+        }
+    }
 }
--- java/baekjoon/gold/PROB7662.java
+++ java/baekjoon/gold/PROB7662.java
@@ -1,4 +1,63 @@
 package gold;
 
+import java.util.*;
+import java.io.*;
+
 public class PROB7662 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int T;
+
+    public static void main(String[] args) throws IOException {
+        init();
+    }
+
+    public static void init() throws IOException {
+        T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+            int Q = Integer.parseInt(br.readLine());
+
+            for (int j = 0; j < Q; j++) {
+                st = new StringTokenizer(br.readLine());
+                String command = st.nextToken();
+                int number = Integer.parseInt(st.nextToken());
+
+                if (command.equals("I"))
+                    map.put(number, map.getOrDefault(number, 0) + 1);
+
+                else if (command.equals("D")) {
+                    if (number == 1 && !map.isEmpty()) {
+                        int max = map.lastKey();
+
+                        if (map.get(max) == 1)
+                            map.remove(max);
+                        else
+                            map.put(max, map.getOrDefault(max, 0) - 1);
+                    }
+
+                    else if (number == -1 && !map.isEmpty()) {
+                        int min = map.firstKey();
+
+                        if (map.get(min) == 1)
+                            map.remove(min);
+                        else
+                            map.put(min, map.getOrDefault(min, 0) - 1);
+                    }
+                }
+            }
+
+            if (map.isEmpty())
+                System.out.println("EMPTY");
+
+            else {
+                int max = map.lastKey();
+                int min = map.firstKey();
+
+                System.out.println(max + " " + min);
+            }
+        }
+    }
 }
